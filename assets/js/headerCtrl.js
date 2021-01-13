5e6164955f11233856ccb5ac1a026be157ea6640
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2021-01-13 18:47:35 -0300

    Add files via upload

--- assets/js/headerCtrl.js
+++ assets/js/headerCtrl.js
@@ -1,10 +1,13 @@
 setInterval(function() {
     let scroll = window.scrollY;
     // 'alt' is a class which will manage the logo and header appareance, from CSS.
-    if(scroll > 150) {
+    if(document.getElementById('sidebar').classList.length > 0) {
+        document.getElementById('header').classList.remove('alt');
+    } else if(scroll > 150) {
         document.getElementById('header').classList.add('alt');
-    } else {
+    }
+    else {
         document.getElementById('header').classList.remove('alt');
     }
-    console.log(scroll);
+    return scroll;
 }, 500); // Doesn't matter this little delay, plus is more CPU efficient
