e9129043f961a87268d5593076cd8defb355b6b5
Santiago Gimenez <42920751+Chiqui1234@users.noreply.github.com>
2021-01-21 17:01:46 -0300

    Almost ready
    # Remaining tasks for Mario
    
    * Animated menu icon in responsive mode (class 'menu removeDesktop') (pending)
    * Section: Works (pending)
    * Section: Flow (Done)
    * Section: Voice and Q&A (in progress)
    * Section: Footer (pending)
    * Section: Blog (pending)
    
    # Remaining tasks for Santiago
    
    * Sidebar for smartphone (Done)
    * Sidebar for tablet (in progress)
    * Section: Contact (Done)
    * Compatibility mode for IE11 (Done : It only remains to change the CSS variables for hard-coded values)
    * Check typos, sizes, margins and revisit entire project (in progress)
    * Image optimizations for mobile and desktop (pending)
    * Home buttons (Done)

--- assets/js/sidebarCtrl.js
+++ assets/js/sidebarCtrl.js
@@ -1,7 +1,19 @@
 function toggleSidebar() {
     let sidebarOpened = document.getElementById('sidebar').classList.length > 0 ? true : false;
-    if(sidebarOpened)
+    if(sidebarOpened) {
         document.getElementById('sidebar').classList.remove('open');
-    else
+        sidebarLock = false;
+    } else {
         document.getElementById('sidebar').classList.add('open');
+        sidebarLock = true;
+    }
+    if(document.getElementById('header').classList.length == 0)
+        document.getElementById('header').classList.add('alt');
+}
+function changeSidebarSubMenuStatus(id) {
+    // Toggles the character inside the icon's submenu
+    document.getElementById(`icon${id}`).innerText == '+' ? document.getElementById(`icon${id}`).innerText = '-' : document.getElementById(`icon${id}`).innerText = '+';
+    // Toggles the submenu in sidebar
+    document.getElementById(`subMenu${id}`).style.display == 'none' ? document.getElementById(`subMenu${id}`).style.display = 'block' : document.getElementById(`subMenu${id}`).style.display = 'none';
+    document.getElementById(`subMenu${id}`).classList[0] == 'trigger' ? document.getElementById(`subMenu${id}`).classList.remove('trigger') : document.getElementById(`subMenu${id}`).classList.add('trigger');
 }
